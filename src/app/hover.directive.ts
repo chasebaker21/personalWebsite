39a2604dda6e28271da484fa907770519afb6eaf
Chase Baker <chasebaker@Chases-MBP.attlocal.net>
2020-04-04 15:30:24 -0400

    added directive Hover for the project image hover effect

--- src/app/hover.directive.ts
+++ src/app/hover.directive.ts
@@ -6,9 +6,11 @@ import { Directive, HostListener } from '@angular/core';
 export class HoverDirective {
 
   constructor() { }
+  
 
+  // adds the image background to each project on hover
   @HostListener('mouseenter', ['$event.target']) onMouseOver(a: any) {
-    a.style.backgroundImage = `url('assets/${a.id}_hover_img')`
+    a.style.backgroundImage = `url('assets/${a.id}.png')`
   }
  @HostListener('mouseleave', ['$event.target']) onMouseLeave(a: any) {
     a.style.backgroundImage = 'initial'
